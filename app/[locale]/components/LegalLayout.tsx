6138151d3bd08f4f4f13837ab622baf1b99dc8d4
JaviLopezV <69149519+JaviLopezV@users.noreply.github.com>
2026-01-10 16:28:45 +0100

    new legallaout

--- app/[locale]/components/LegalLayout.tsx
+++ app/[locale]/components/LegalLayout.tsx
@@ -1,4 +1,6 @@
-import { Box, Container, Typography } from "@mui/material";
+"use client";
+import { Box, Container, Typography, Button } from "@mui/material";
+import { Link } from "@/i18n/navigation";
 
 export default function LegalLayout({
   title,
@@ -9,9 +11,22 @@ export default function LegalLayout({
 }) {
   return (
     <Container maxWidth="md" sx={{ py: 6 }}>
+      {/* Volver */}
+
+      <Button
+        component={Link as any}
+        variant="outlined"
+        size="large"
+        href="/auth"
+        sx={{ mb: 2 }}
+      >
+        Volver
+      </Button>
+
       <Typography variant="h4" fontWeight={800} gutterBottom>
         {title}
       </Typography>
+
       <Box sx={{ color: "text.secondary", lineHeight: 1.7 }}>{children}</Box>
     </Container>
   );
